d3402e396f549dd498d17cfd31b846223611c075
Awolad Hossain <awolad1122@gmail.com>
2019-07-10 16:44:24 +0600

    fix

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {selectBook} from "../actions";
-import {bindActionCreators} from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions";
+import { bindActionCreators } from "redux";
 
 class BookList extends Component {
-    renderList() {
-        return this.props.books.map((book) => {
-            return (
-                <li
-                    onClick={() => this.props.selectBook(book)}
-                    key={book.title}
-                    className="list-group-item">
-                    {book.title}
-                    </li>
-            );
-        });
-    }
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li
+          onClick={() => this.props.selectBook(book)}
+          key={book.title}
+          className="list-group-item"
+        >
+          {book.title}
+        </li>
+      );
+    });
+  }
 
-    render() {
-        return (
-            <ul className="list-group col-sm-4">
-                {this.renderList()}
-            </ul>
-        );
-    }
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        books: state.books
-    }
+  return {
+    books: state.books
+  };
 }
 
 function mapDisptachToPorps(dispatch) {
-    return bindActionCreators({selectBook: selectBook}, dispatch);
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDisptachToPorps)(BookList);
+export default connect(
+  mapStateToProps,
+  mapDisptachToPorps
+)(BookList);
